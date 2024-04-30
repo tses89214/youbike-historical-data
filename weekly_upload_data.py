55159a521ec7b6e8bf8f62715473a7c94f97bd81
Chia-Xuan Jian <tses89214@gmail.com>
2024-04-30 21:30:49 +0800

    fix: make upload process work

--- weekly_upload_data.py
+++ weekly_upload_data.py
@@ -2,6 +2,8 @@
 This script used to export sites and slots data WEEKLY.
 """
 import datetime
+import dotenv
+import os
 
 from src.db_connector import BQConnector
 
@@ -10,12 +12,16 @@ def main():
     """
     export sites and slots data to csv weekly.
     """
+    dotenv.load_dotenv(override=True)
     connector = BQConnector()
-    connector.read_sites().to_csv('data/sites/sites.csv')
 
-    today_slots = datetime.datetime.strftime('slots/%Y-%m-%d.csv')
-    connector.read_slots().to_csv(f'data/{today_slots}')
+    os.makedirs('data/sites', exist_ok=True)
+    os.makedirs('data/slots', exist_ok=True)
+
+    connector.read_sites().to_csv('data/sites/sites.csv', index=False)
+    today_slots = datetime.datetime.now().strftime('slots/%Y-%m-%d.csv')
+    connector.read_slots().to_csv(f'data/{today_slots}', index=False)
+
 
 if __name__ == "__main__":
     main()
-    
