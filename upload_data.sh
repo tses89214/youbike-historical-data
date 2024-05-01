ead1b1675465f56533e95c8f1bce974dcf8cfb1c
Chia-Xuan Jian <tses89214@gmail.com>
2024-05-01 23:26:21 +0800

    Fix: modify commit message format

--- upload_data.sh
+++ upload_data.sh
@@ -3,5 +3,5 @@ date=$(date -d "2 days ago" +%Y-%m-%d)
 git pull
 /home/tses89214/ubike-historical-data/my_venv/bin/python3 /home/tses89214/ubike-historical-data/daily_upload_data.py
 git add data/*
-git commit -m "chore: auto add data $date"
+git commit -m "Data: auto add data $date"
 git push
