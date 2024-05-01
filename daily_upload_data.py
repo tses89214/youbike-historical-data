771092425e97cd2b33221d79acf9a050b9724ae1
Chia-Xuan Jian <tses89214@gmail.com>
2024-05-01 22:19:07 +0800

    Feat: basic alarm (temp)

--- daily_upload_data.py
+++ daily_upload_data.py
@@ -2,28 +2,38 @@
 This script used to export sites and slots data WEEKLY.
 """
 import datetime
-import dotenv
 import os
+import traceback
+
+import dotenv
 
 from src.db_connector import BQConnector
+from src.alarm import TGBot
 
 
 def main():
     """
     export sites and slots data to csv weekly.
     """
-    dotenv.load_dotenv(override=True)
-    connector = BQConnector()
+    try:
+        dotenv.load_dotenv(override=True)
+        connector = BQConnector()
 
-    os.makedirs('data/sites', exist_ok=True)
-    os.makedirs('data/slots', exist_ok=True)
+        os.makedirs('data/sites', exist_ok=True)
+        os.makedirs('data/slots', exist_ok=True)
 
-    connector.read_sites().to_csv('data/sites/sites.csv', index=False)
+        connector.read_sites().to_csv('data/sites/sites.csv', index=False)
 
-    date = datetime.datetime.now() - datetime.timedelta(days=2)
-    slots_filename = date.strftime('data/slots/%Y-%m-%d.csv')
-    connector.read_slots().to_csv(slots_filename, index=False)
-    connector.clean_slots()
+        date = datetime.datetime.now() - datetime.timedelta(days=2)
+        slots_filename = date.strftime('data/slots/%Y-%m-%d.csv')
+        connector.read_slots().to_csv(slots_filename, index=False)
+        connector.clean_slots()
+
+    # pylint:disable=broad-exception-caught
+    except Exception:
+        dotenv.load_dotenv(override=True)
+        bot = TGBot()
+        bot.send_message(traceback.format_exc())
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -2,35 +2,45 @@
 Here is the main entrypoint of this project.
 """
 import hashlib
+import traceback
+
 import dotenv
 
 from src.crawler import download_data, split_table
 from src.db_connector import BQConnector
+from src.alarm import TGBot
 
 
 def main():
     """
     Main entrypoint.
     """
-    dotenv.load_dotenv(override=True)
+    try:
+        dotenv.load_dotenv(override=True)
 
-    bq_connector = BQConnector()
+        bq_connector = BQConnector()
 
-    records = download_data()
-    sites, slots = split_table(records)
+        records = download_data()
+        sites, slots = split_table(records)
 
-    # generate current md5 for check.
-    m = hashlib.md5()
-    m.update(sites.to_string().encode())
-    current_md5 = m.hexdigest()
-    md5_check_result = bq_connector.check_md5_for_update(current_md5)
+        # generate current md5 for check.
+        m = hashlib.md5()
+        m.update(sites.to_string().encode())
+        current_md5 = m.hexdigest()
+        md5_check_result = bq_connector.check_md5_for_update(current_md5)
 
-    if md5_check_result is False:
-        bq_connector.overwrite_sites(sites)
-        bq_connector.overwrite_site_md5(current_md5)
+        if md5_check_result is False:
+            bq_connector.overwrite_sites(sites)
+            bq_connector.overwrite_site_md5(current_md5)
 
-    # slots always update.
-    bq_connector.append_slots(slots)
+        # slots always update.
+        bq_connector.append_slots(slots)
+
+    # pylint:disable=broad-exception-caught
+    except Exception:
+        dotenv.load_dotenv(override=True)
+        bot = TGBot()
+        bot.send_message(traceback.format_exc())
 
 
 if __name__ == "__main__":
