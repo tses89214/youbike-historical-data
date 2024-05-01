9314a6d93560834b6924c29c12b53a6cf988072d
Chia-Xuan Jian <tses89214@gmail.com>
2024-05-01 23:03:39 +0800

    Fix: remove unused vars

--- upload_data.sh
+++ upload_data.sh
@@ -1,11 +1,7 @@
 cd /home/tses89214/ubike-historical-data
-
 date=$(date -d "2 days ago" +%Y-%m-%d)
-branch_name=upload-data-$date
-
 git pull
 /home/tses89214/ubike-historical-data/my_venv/bin/python3 /home/tses89214/ubike-historical-data/daily_upload_data.py
 git add data/*
-
 git commit -m "chore: auto add data $date"
 git push
